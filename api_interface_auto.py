cd528b0e50688c7a20dd4c0f45bb58240a63074a
HuangSGH <273227864@qq.com>
2017-08-28 13:24:11 +0800

    写好了，上传一下，写的是之前丢失的接口调用脚本，还得优化，先暂时这样用。

--- api_interface_auto.py
+++ api_interface_auto.py
@@ -34,13 +34,13 @@ def risk_report_all_sen():
     # url_risk = 'http://192.168.1.113/api/risk'
     # url_report = 'http://192.168.1.113/api/logreport/businesslog'
     # 测试地址
-    # url_monitor = 'http://192.168.1.112:6080/api/emulator'
-    # url_risk = 'http://192.168.1.112:6080/api/risk'
-    # url_report = 'http://192.168.1.112:6080/api/logreport/businesslog'
+    url_monitor = 'http://192.168.1.112:6080/api/emulator'
+    url_risk = 'http://192.168.1.112:6080/api/risk'
+    url_report = 'http://192.168.1.112:6080/api/logreport/businesslog'
     # 阿里云nginx地址
-    url_monitor = 'http://ifds1.trusfort.com/api/emulator'
-    url_risk = 'http://ifds1.trusfort.com/api/risk'
-    url_report = 'http://ifds1.trusfort.com/api/logreport/businesslog'
+    # url_monitor = 'http://ifds1.trusfort.com/api/emulator'
+    # url_risk = 'http://ifds1.trusfort.com/api/risk'
+    # url_report = 'http://ifds1.trusfort.com/api/logreport/businesslog'
     # 阿里云Trusfort5地址
     # url_monitor = 'http://ifds1.trusfort.com:8080/api/emulator'
     # url_risk = 'http://ifds1.trusfort.com:8080/api/risk'
